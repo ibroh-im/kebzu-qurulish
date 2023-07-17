882458c24d449c8812c11db78d608b453e198ddc
ibroh-im <ibrohimabduganiyev184@gmail.com>
2023-07-17 17:55:00 +0500

    all is finished?

---
+++ assets/custom/js/map.js
@@ -0,0 +1,7 @@
+// function init() {
+//     let map = new ymaps.Map('map-test',{
+//         center:[41.34500724530861,69.28296451574703],
+//         zoom:
+//     });
+// }
+// ymaps.ready(init);
